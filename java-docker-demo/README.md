c38aaa1b595b4dc9ceb5d77dae95b1e59e9771d8
今天幸福不在家 <zhujwmail@163.com>
2023-11-20 01:42:25 +0800

    SpringBoot项目创建Docker

---
+++ java-docker-demo/README.md
@@ -0,0 +1,18 @@
+# 学习docker镜像构建
+
+环境准备：
+
+- redis
+- docker环境
+
+笔记：
+https://www.cnblogs.com/zjw-blog/category/1886775.html
+
+
+参考：
+
+教程文档：
+https://www.yuque.com/leifengyang/oncloud/mbvigg
+
+视频教程：
+https://www.bilibili.com/video/BV13Q4y1C7hS?p=8
---
+++ java-docker-demo/src/main/java/com/zjw/docker/controller/CounterController.java
@@ -0,0 +1,28 @@
+package com.zjw.docker.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 朱俊伟
+ * @date 2023/11/01 19:48
+ */
+@RestController
+public class CounterController {
+
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
+    @GetMapping("/")
+    public String count() {
+        Long countPeople = redisTemplate.opsForValue().increment("count-people");
+        return "有【"+ countPeople +"】人访问该页面";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+}
